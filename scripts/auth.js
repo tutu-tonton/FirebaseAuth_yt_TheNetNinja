43e938d5091e9a851c63ac4abf463386c771b9af
tutu-tonton <tngt33844507@gmail.com>
2020-06-08 17:09:34 +0900

    lesson7 login機能実装

--- scripts/auth.js
+++ scripts/auth.js
@@ -8,21 +8,11 @@ const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	//========================================
 	//  get user info
-	//
-	//
-	//
-	//========================================
 	const email = signupForm['signup-email'].value;
 	const password = signupForm['signup-password'].value;
 
-	//========================================
 	//  sign up the user
-	//
-	//
-	//
-	//========================================
 	auth.createUserWithEmailAndPassword(email, password).then((cred) => {
 		// signupに成功するとtokenが返ってくる.
 		// console.log(cred);
@@ -47,5 +37,28 @@ logout.addEventListener('click', (e) => {
 	e.preventDefault();
 	auth.signOut().then(() => {
 		console.log('user signed out');
+	});
+});
+
+//========================================
+//  login
+//
+//
+//========================================
+
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	//get user info
+	const email = loginForm['login-email'].value;
+	const password = loginForm['login-password'].value;
+
+	auth.signInWithEmailAndPassword(email, password).then((cred) => {
+		console.log(cred.user);
+		// close the login modal and reset the form
+		const modal = document.querySelector('#modal-login');
+		M.Modal.getInstance(modal).close();
+		loginForm.reset();
 	});
 });
